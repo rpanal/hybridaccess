be34b428c6eeb368d9f1ade268d8b4929e84ce7a
rpanal <restiep@gmail.com>
2024-10-15 13:30:55 +0800

    Add files via upload

--- CCLPIHybrid.py
+++ CCLPIHybrid.py
@@ -1,32 +1,39 @@
+from sqlalchemy import ColumnElement, true
 import streamlit as st
 import pyodbc
 import pandas as pd
 import numpy as np
-
-
+import streamlit as st
+#from multipage import MultiPage
+#from pages import page1, page2
  
-#conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=D:\CCLPIHybridweb\Database3_be.accdb;')
-#users_agent = 'SELECT users.userid, users.username, agents.AgentID FROM agents INNER JOIN users ON agents.AgentID = users.agent_ID;'
+conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=.\Data\Database3_be.accdb;')
 
-#cursor = conn.cursor(conn)
+relate = 'SELECT username, password FROM users ORDER BY username;'
 
-#cursor.execute('SELECT username, userid from users')
+relatefile = pd.read_sql(relate,conn,)
 
-# Title
-st.title("Login Page")
-
-# Username input
-username = st.text_input("Username")
-
-# Password input
-password = st.text_input("Password", type="password")
-
-# Login button
-if st.button("Login"):
-    if username == "admin" and password == "password":
-        st.success("Logged in successfully!")
-    else:
-        st.error("Invalid username or password")
+# Main
 
 
+# Title with Markdown formatting
+
+st.markdown("# CCLPI - Hybrid ")
+
+# Add a title to the sidebar
+
+# Add a selectbox to the sidebar
+option = st.sidebar.selectbox(
+    "Select an option:",
+    ["Option 1", "Option 2", "Option 3"]
+)
+
+#app = MultiPage()
+
+# Add pages to the app
+#app.add_page("Page 1", page1.app)
+#app.add_page("Page 2", page2.app)
+
+# Run the app
+#app.run()
 
