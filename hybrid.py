ba00b92c3f3771978d8a46c5c4ef0fb4241c523c
rpanal <restiep@gmail.com>
2024-10-15 14:31:43 +0800

    Add files via upload

--- hybrid.py
+++ hybrid.py
@@ -1,48 +1,39 @@
-
+from sqlalchemy import ColumnElement, true
 import streamlit as st
+import pyodbc
 import pandas as pd
-import pyodbc as msacc
-from PIL import Image
+import numpy as np
+import streamlit as st
+#from multipage import MultiPage
+#from pages import page1, page2
+ 
+conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=.\Data\Database3_be.accdb;')
 
-#import cv2
+relate = 'SELECT username, password FROM users ORDER BY username;'
 
-cnn_string = (r"Driver=(Microsoft Access driver (*.mdb, *.accdb));")
-st.title("Restie")
+relatefile = pd.read_sql(relate,conn,)
 
-
-col1, col2, col3 , col4 = st.columns([1,2,3,4])
-col1.markdown ("Welcome to my Application")
-col1.markdown ("Just for Samples")
-
-
-col3.markdown("Just for Samples 3")
-upload_soundfile = col4.file_uploader("Upload Audio File")
-
-uploaded_photo = col3.file_uploader("upload a photo")                                                                                      
-camera_photo = col2.camera_input("upload a photo from Camera",)
-st.write("""# My first appn Hello *world!*""")
-
-myname = col3.text_input("Enter your name")
-col3.markdown (myname)
-
-col4.markdown("")
-
-if uploaded_photo is not None and camera_photo is not None:
-    st.image(uploaded_photo,"My Image",300) 
-    st.sidebar.image (camera_photo,caption = "Me",width = 100,clamp=True,output_format ="auto")
-    st.sidebar.image (uploaded_photo)
-
-st.form("Restie",border=True)
-
-img = Image.open("./agentpics./user.png")
-newimage = st.image(img,width=30)
-st.sidebar.button("Reset", type="primary")
+# Main
 
 
-if st.sidebar.button("Say hello"):
-    
-    st.write("Why hello there")
-else:
-    st.write("Hello")
+# Title with Markdown formatting
 
+st.markdown("# CCLPI - Hybrid ")
+
+# Add a title to the sidebar
+
+# Add a selectbox to the sidebar
+option = st.sidebar.selectbox(
+    "Select an option:",
+    ["Option 1", "Option 2", "Option 3"]
+)
+
+#app = MultiPage()
+
+# Add pages to the app
+#app.add_page("Page 1", page1.app)
+#app.add_page("Page 2", page2.app)
+
+# Run the app
+#app.run()
 
